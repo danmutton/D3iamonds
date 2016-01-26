5f8a404b370748c0e22645e99ff8e997bc877f58
Dan Mutton <daniel.mutton@gmail.com>
2016-01-26 21:33:10 +1100

    Ultimately just a cubic zirconia

---
+++ go.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo ""
+echo "You should just open the file in your browser"
+echo "" 
+echo "But you should be online to pull d3 from the web (otherwise things will be a little quiet)"
+echo ""
+exit 1
